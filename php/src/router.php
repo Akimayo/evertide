e344edee724929e5882deec46da9911db54a38dc
Michal Ciesla <ciesla.m@outlook.com>
2025-06-01 15:49:02 +0200

    Add MIME type handling to router

--- php/src/router.php
+++ php/src/router.php
@@ -14,5 +14,12 @@ $script = $query_pos ? substr($_SERVER['REQUEST_URI'], 1, $query_pos) : substr($
 if (empty($script)) $script = 'index.php';
 $path = dirname(__DIR__) . DIRECTORY_SEPARATOR . $script;
 if (empty(pathinfo($script, PATHINFO_EXTENSION))) $path .= '.php';
-if (file_exists($path)) include($path);
-else header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found');
+if (file_exists($path)) {
+    $ext = pathinfo($path, PATHINFO_EXTENSION);
+    switch ($ext) {
+        case 'css':
+            header('Content-Type: text/css', true);
+            break;
+    }
+    include($path);
+} else header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found');
