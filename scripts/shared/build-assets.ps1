51645058078e44543347f8680a65a0ef038d10ce
Michal Ciesla <ciesla.m@outlook.com>
2025-07-01 19:13:08 +0200

    Show percentage progress when building assets

--- scripts/shared/build-assets.ps1
+++ scripts/shared/build-assets.ps1
@@ -1,7 +1,8 @@
 . ($PSScriptRoot + "\install-dependencies.ps1")
 
 # Run tsc
-Write-Progress "Building JavaScript"
+$progressMessage = "Building JavaScript"
+Write-Progress $progressMessage
 $p = Start-Process "node" -ArgumentList ($WEB_PATH + ".\node_modules\typescript\bin\tsc") -WorkingDirectory $WEB_PATH -RedirectStandardError $ERROR_FILE -NoNewWindow -Wait -PassThru
 If ($p.ExitCode -ne 0) {
     Write-Error "tsc exitted with error $($p.ExitCode)"
@@ -10,7 +11,12 @@ If ($p.ExitCode -ne 0) {
     Write-Progress "evertide" -Completed
     Exit
 }
-Get-ChildItem ($WEB_PATH + "*.js") | ForEach-Object {
+Write-Progress $progressMessage -PercentComplete 50
+$items = Get-ChildItem ($WEB_PATH + "*.js")
+$itemPercent = 50.0 / $items.Length
+$i = 0
+$items | ForEach-Object {
+    Write-Progress $progressMessage -PercentComplete (50 + ($i++) * $itemPercent) -Status "Minifying $($_.Name)"
     If ($_.FullName.Substring($_.FullName.Length - 7) -eq ".min.js") { Return }
     $minPath = $_.FullName.Substring(0, $_.FullName.Length - 3) + ".min.js";
     $p = Start-Process "node" -ArgumentList ($WEB_PATH + ".\node_modules\jsmin\bin\jsmin"), $_.FullName -WorkingDirectory $WEB_PATH -RedirectStandardError $ERROR_FILE -RedirectStandardOutput $minPath -NoNewWindow -Wait -PassThru
@@ -25,11 +31,15 @@ Get-ChildItem ($WEB_PATH + "*.js") | ForEach-Object {
         "//# sourceMappingURL=$($_.Name).map" | Out-File $minPath -Append -Encoding utf8
     }
 }
-Write-Host "Built and minified JavaScript"
 
 # Run sass
-Write-Progress "Building CSS"
-Get-ChildItem ($WEB_PATH + "*.scss") | ForEach-Object {
+$progressMessage = "Building CSS"
+Write-Progress $progressMessage
+$items = Get-ChildItem ($WEB_PATH + "*.scss")
+$itemPercent = 100.0 / $items.Length
+$i = 0
+$items | ForEach-Object {
+    Write-Progress $progressMessage -PercentComplete (($i++) * $itemPercent) -Status "Compiling $($_.Name)"
     If ($_.Name.StartsWith("_")) { Return; }
     $minPath = $_.FullName.Substring(0, $_.FullName.Length - 5) + ".min.css";
     $p = Start-Process "node" -ArgumentList ($WEB_PATH + ".\node_modules\sass\sass.js"), "$($_.FullName):$($minPath)", "--style", "compressed" -WorkingDirectory $WEB_PATH -RedirectStandardError $ERROR_FILE -NoNewWindow -Wait -PassThru
@@ -41,7 +51,6 @@ Get-ChildItem ($WEB_PATH + "*.scss") | ForEach-Object {
         Exit
     }
 }
-Write-Host "Built and minified CSS"
 
 Remove-Item $ERROR_FILE -ErrorAction SilentlyContinue
 Write-Progress "evertide" -Completed
