0437dc407073df1432787a46b97fadba97c992d6
Michal Ciesla <ciesla.m@outlook.com>
2025-07-23 13:44:39 +0200

    Improve tab navigation - Add styling for categories and links - Add handler for closing categories with the <kbd>Esc</kbd> key

--- web/toggle.ts
+++ web/toggle.ts
@@ -7,3 +7,10 @@ window.addEventListener('load', () =>
                     .forEach((elmt: HTMLDetailsElement) => elmt.isEqualNode(c) || (elmt.open = false))
             ))
 );
+window.addEventListener('keydown', (evt) => {
+    let elmt: HTMLDetailsElement;
+    if (evt.key === "Escape" && (elmt = document.querySelector('main article>details[open]'))) {
+        elmt.open = false;
+        (elmt.children[0] as HTMLElement).focus();
+    }
+});
