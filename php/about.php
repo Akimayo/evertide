ddd66219a475403e282daa3c11fda5653ca0c41b
Michal Ciesla <ciesla.m@outlook.com>
2025-08-03 16:04:37 +0200

    Add localization credits to _/about_ page

--- php/about.php
+++ php/about.php
@@ -2,4 +2,20 @@
 
 /** @var Handler $handler */
 $handler = require(__DIR__ . '/src/handler.php');
-$handler->render('about.latte', ['instances' => InstanceDAO::getAll($handler->getDatabase())]);
+
+$authorNames = explode(';', L::about_development_localization_authorNames);
+$authorUrls = explode(';', L::about_development_localization_authorUrls);
+$authors = [];
+foreach ($authorNames as $i => $name) {
+    $name = trim($name);
+    $url = trim($authorUrls[$i]);
+    $authors[] = [
+        $name,
+        empty($url) || $url == '-' ? null : $url
+    ];
+}
+
+$handler->render('about.latte', [
+    'instances' => InstanceDAO::getAll($handler->getDatabase()),
+    'authors' => $authors
+]);
