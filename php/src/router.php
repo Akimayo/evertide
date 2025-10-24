2637fec4cc5af4e01a1158b3d0a5cb215a7ef28e
Michal Ciesla <ciesla.m@outlook.com>
2025-10-24 17:14:54 +0200

    Add multi-instance support for routing - Check whether first part of URL pathname exists as an instance in the _opt/_ directory - If so, set the `EVERTIDE_INSTANCE` environment variable and process request in the parent script files

--- php/src/router.php
+++ php/src/router.php
@@ -11,6 +11,13 @@
  */
 $query_pos = strpos($_SERVER['REQUEST_URI'], '?');
 $script = $query_pos ? substr($_SERVER['REQUEST_URI'], 1, $query_pos - 1) : substr($_SERVER['REQUEST_URI'], 1);
+$first_slash = strpos($script, '/');
+if ($first_slash === false) $first_slash = null;
+if (!empty($script) && file_exists(dirname(__DIR__) . DIRECTORY_SEPARATOR . 'opt' . DIRECTORY_SEPARATOR . substr($script, 0, $first_slash))) {
+    $_SERVER['EVERTIDE_INSTANCE'] = substr($script, 0, $first_slash);
+    putenv('EVERTIDE_INSTANCE=' . $_SERVER['EVERTIDE_INSTANCE']);
+    $script = is_null($first_slash) ? '' : substr($script, $first_slash + 1);
+}
 if (empty($script)) $script = 'index.php';
 $path = dirname(__DIR__) . DIRECTORY_SEPARATOR . $script;
 if (empty(pathinfo($script, PATHINFO_EXTENSION))) $path .= '.php';
